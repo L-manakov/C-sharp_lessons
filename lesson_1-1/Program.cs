afde77b84570c83ef979290c363a4f5f531d2a8c
Leonid <leonid.manakov@gmail.com>
2023-01-13 15:37:40 +0300

    Intro function

--- lesson_1-1/Program.cs
+++ lesson_1-1/Program.cs
@@ -1,4 +1,15 @@
-﻿Console.Write("Введите Ваше имя ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.WriteLine(username);
+﻿Console.Write("Введите число a = ");
+int a = Int32.Parse(Console.ReadLine());
+
+Console.Write("Введите число b = ");
+int b = Int32.Parse(Console.ReadLine());
+
+var max = a;
+var min = b;
+
+if (a > b) max = a;
+
+    Console.Write("max = ");
+    Console.WriteLine(a);
+    Console.Write("min = ");
+    Console.WriteLine(b);
