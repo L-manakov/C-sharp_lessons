cd3cf1d839e68b2b525516207e2c59da3e496003
Leonid <leonid.manakov@gmail.com>
2023-01-16 03:55:10 +0300

    Third lesson - Methods

---
+++ lesson_3-2_ArrangeArray/Program.cs
@@ -0,0 +1,58 @@
+﻿// Алгоритм сортировки методом выбора (Min и Max)
+
+// 1. Найти позицию минимального элемента в неотсоритрованной части массива
+// 2. Произвести обмен этого значения со значением первой неотсортированной позиции
+// 3. Повторить пока есть не отсортированные элементы
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)    // метод вывода массива на экран
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSortUp(int[] array) // метод упорядочивания массива от min к max
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j; // поиск min
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+void SelectionSortDown(int[] array) // метод упорядочивания массива от max к min
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j; // поиск max
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+
+SelectionSortUp(arr);
+PrintArray(arr);
+
+SelectionSortDown(arr);
+PrintArray(arr);
---
+++ lesson_3/Program.cs
@@ -0,0 +1,89 @@
+﻿// Вид 1 - Методы, которые ничего не возвращают и не принимают.
+
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+//Method1();  // Вызываются идентификатором метода с круглыми скобками
+
+
+
+// Вид 2 - Методы, которые ничего не возвращают, но могут принимать какие-то аргументы
+
+void Method2(string msg)
+{
+     Console.WriteLine(msg);
+}
+//Method2(msg: "Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;        // увеличении числа на +1 - инкремент
+    }
+}
+//Method21(msg: "Текст", count: 4);   // "текст" - аргумент, 4 -количество выводов
+
+
+
+// Вид 3 - Методы, которые что-то возвращают, но ничего не принимают
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+
+
+// Вид 4 - Методы, которые что-то принимают и что-то возвращают
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty; // изначально присваиваем результату пустую строку
+
+    while (i < count)
+    {
+        result = result + text;        
+        i++;
+    }     
+    return result;  // оператор return возвращает переменную, которую мы ожидаем получить из метода
+}
+string res = Method4(10, "qwerty ");
+Console.WriteLine(res);
+
+
+
+// Цикл for
+
+string Method41(int count, string text)
+{
+    string result = String.Empty; // изначально присваиваем результату пустую строку
+    for(int i = 0; i < count;  i++)
+    {
+        result = result + text;        
+    }     
+    return result;  // оператор return возвращает переменную, которую мы ожидаем получить из метода
+}
+string res41 = Method41(10, "qw ");
+Console.WriteLine(res41);
+
+
+
+// Цикл в цикле
+// Пример - вывод таблицы умножения на экран
+
+for (int i = 2; i <= 10; i++)       // внешний цикл
+{
+    for (int j = 0;  j <= 10;  j++) // оператор или тело метода
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}"); // выводим через интерполяцию строк
+    }
+    Console.WriteLine();            // разрыв строки между циклами
+}
+
