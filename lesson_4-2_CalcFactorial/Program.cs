54203f3b8d57ef7fab4e0f780cd75c5cf7e9cce7
Leonid <leonid.manakov@gmail.com>
2023-01-17 20:12:57 +0300

    Lesson 4

---
+++ lesson_4-2_CalcFactorial/Program.cs
@@ -0,0 +1,12 @@
+﻿int Factorial(int n)
+{
+    if(n==1) return 1;
+    else return n * Factorial(n -1);
+}
+for (int i = 0; i < 40; i++)
+{
+Console.WriteLine($"{i}! = {Factorial(i)}"); // i - аргумент факториала   
+}
+
+// привычеслении больших факториалов (!20) int переполняется
+// можно заменить на double
---
+++ lesson_4_RecursionAlgorithm/Program.cs
@@ -0,0 +1,50 @@
+﻿// Общий вид матрицы
+
+// string[,] table = new string [2, 5];
+// String.Empty - по умолчанию
+// обращение к элементам матрицы
+// table[0,0]   table[0,1]  table[0,2]  table[0,3]  table[0,4]
+// table[1,0]   table[1,1]  table[1,2]  table[1,3]  table[1,4]
+// table[1, 2] = "слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"--{table[rows, columns]}--");
+//     }   
+// }
+
+
+// Метод вывода матрицы на экран
+
+void PrintArray(int[,] matr)    // в качестве агумента вумерная таблица чисел
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+// Метод заполнения матрицы случайными числами
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);    // генерирует числа от 1 до 10
+        }
+    }
+}
+
+int[,] matrix = new int[3,4];
+PrintArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
+PrintArray(matrix);
